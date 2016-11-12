cc83e81ea70cab32d3008a923552f509102f1779
Mackenzie Hicks <hicks.264@gmail.com>
2016-11-11 17:11:25 -0800

    reordered routes

--- test/controllers/stripe_account_controller_test.exs
+++ test/controllers/stripe_account_controller_test.exs
@@ -12,22 +12,31 @@ defmodule CodeCorps.StripeAccountControllerTest do
       |> assert_ids_from_response([stripe_account_1.id, stripe_account_2.id])
     end
 
-    # test "renders 401 when not authenticated", %{conn: conn} do
-    #   assert conn |> request_index |> json_response(401)
-    #
-    # end
-    #
-    # @tag :authenticated
-    # test "renders 403 when not authorized", %{conn: conn} do
-    #   assert conn |> request_index |> json_response(403)
-    #
-    # end
+    test "renders 401 when not authenticated", %{conn: conn} do
+      assert conn |> request_index |> json_response(401)
+
+    end
+
+    @tag :authenticated
+    test "renders 403 when not authorized", %{conn: conn} do
+      assert conn |> request_index |> json_response(403)
+
+    end
   end
 
 
-  # describe "show" do
-  #
-  # end
+  describe "show" do
+    test "shows chosen resource", %{conn: conn} do
+      stripe_account = insert(:stripe_account)
+
+      conn
+      |> request_show(stripe_account)
+      |> json_response(200)
+      |> Map.get("data")
+      |> assert_result_id(stripe_account.id)
+    end
+    
+  end
 
 
 
