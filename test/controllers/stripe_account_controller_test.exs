7ce3483b1ad656d6402fe78c36f0d9c4151a28ac
Mackenzie Hicks <hicks.264@gmail.com>
2016-11-15 16:38:34 -0800

    added new show test for 401 response

--- test/controllers/stripe_account_controller_test.exs
+++ test/controllers/stripe_account_controller_test.exs
@@ -12,5 +12,10 @@ defmodule CodeCorps.StripeAccountControllerTest do
       |> Map.get("data")
       |> assert_result_id(stripe_account.id)
     end
+
+    test "renders 401 when unauthenticated", %{conn: conn} do
+      stripe_account = insert(:stripe_account)
+      assert conn |> request_show(stripe_account) |> json_response(401)
+    end
   end
 end
